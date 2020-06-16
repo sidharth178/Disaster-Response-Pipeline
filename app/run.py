47ad31f2bb8e95759385e0b68034fc57da451f3c
SIDHARTH KUMAR MOHANTY <65949218+sidharth178@users.noreply.github.com>
2020-06-17 01:21:25 +0530

    Add files via upload

--- app/run.py
+++ app/run.py
@@ -1,9 +1,12 @@
-# import all the libraries
+# import all libraries here
 import json
 import plotly
 import pandas as pd
+import nltk
 from nltk.stem import WordNetLemmatizer
-from nltk.tokenize import word_tokenize
+from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk import pos_tag, word_tokenize
+from sklearn.base import BaseEstimator, TransformerMixin
 from flask import Flask
 from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
@@ -11,32 +14,59 @@ from sklearn.externals import joblib
 from sqlalchemy import create_engine
 
 app = Flask(__name__)
+
+class StartingVerbExtractor(BaseEstimator, TransformerMixin):
+    def starting_verb(self, text):
+        ''' We will extract verbs from the text after tokenizing them'''
+        li = nltk.sent_tokenize(text)
+        for sentence in li:
+            pos_tags = nltk.pos_tag(tokenize(sentence))
+            first_word, first_tag = pos_tags[0]
+            if first_tag in ['VB', 'VBP'] or first_word == 'RT':
+                return True
+        return False
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        X_tagged = pd.Series(X).apply(self.starting_verb)
+        return pd.DataFrame(X_tagged)
+
 def tokenize(text):
+    '''In this function we will tokenize the text'''
     tokens = word_tokenize(text)
     lemmatizer = WordNetLemmatizer()
+
     clean_tokens = []
     for tok in tokens:
         clean_tok = lemmatizer.lemmatize(tok).lower().strip()
         clean_tokens.append(clean_tok)
+
     return clean_tokens
-
-# load the data here
+#Here we lode the data
 engine = create_engine('sqlite:///../data/DisasterResponse.db')
-df = pd.read_sql_table('DisasterResponse', engine)
+df = pd.read_sql_table('df', engine)
 
-# load the model here
+#Here we load the model!
 model = joblib.load("../models/classifier.pkl")
 
+
+# index webpage receives the user input text for the model and displays the cool visuals 
 @app.route('/')
 @app.route('/index')
 def index():
-    
-    # extract these data that needed for visuals
+    ''' Here we extract data that needed for visuals! '''
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
     
-    # create the visuals
+    category_names = df.iloc[:,4:].columns
+    category_boolean = (df.iloc[:,4:] != 0).sum().values
+    
+# here create the visuals    
+   
     graphs = [
+
         {
             'data': [
                 Bar(
@@ -46,7 +76,7 @@ def index():
             ],
 
             'layout': {
-                'title': 'Distribution of Message Genres',
+                'title': ' Message Genres',
                 'yaxis': {
                     'title': "Count"
                 },
@@ -54,23 +84,44 @@ def index():
                     'title': "Genre"
                 }
             }
+        },
+  
+        {
+            'data': [
+                Bar(
+                    x=category_names,
+                    y=category_boolean
+                )
+            ],
+
+            'layout': {
+                'title': 'Message Categories',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Category",
+                    'tickangle': 35
+                }
+            }
         }
     ]
     
-    
+    # Here we encoded the plotly graphs in JSON
     ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
     graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
+    
     return render_template('master.html', ids=ids, graphJSON=graphJSON)
 
 @app.route('/go')
 def go():
-    # save the user input in the query
+    '''Will push the Query through this function '''
     query = request.args.get('query', '') 
 
-    # use the model to predict the classification for queries
     classification_labels = model.predict([query])[0]
     classification_results = dict(zip(df.columns[4:], classification_labels))
- 
+    
+   
     return render_template(
         'go.html',
         query=query,
@@ -79,7 +130,7 @@ def go():
 
 
 def main():
-    app.run(host='0.0.0.0', port=3001, debug=True)
+    app.run(host='127.0.0.1', port=5000, debug=True)
 
 
 if __name__ == '__main__':
