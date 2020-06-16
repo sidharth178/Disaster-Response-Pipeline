84e57131f8c3c37f65ac1768043882c8318ed6c6
SIDHARTH KUMAR MOHANTY <65949218+sidharth178@users.noreply.github.com>
2020-06-17 01:22:19 +0530

    Add files via upload

--- data/process_data.py
+++ data/process_data.py
@@ -1,89 +1,58 @@
-# import all the libraries here
+# import all libraries here
 import sys
 import pandas as pd
 from sqlalchemy import create_engine
 
-def load_data(messages_filepath="./data/disaster_messages.csv",
-              categories_filepath="./data/disaster_categories.csv"):
-    '''Load csv files into dataframes and return'''
-    messages_df = pd.read_csv(messages_filepath)
-    categories_df = pd.read_csv(categories_filepath)
-    return messages_df, categories_df
-
-def clean_data(messages_df, categories_df):
-    ''''
-    INPUT
-    messages_df - pandas dataframe
-    categories_df - pandas dataframe
-
-    OUTPUT
-    df - a cleaned and concatenated dataframes from two inputs
-
-    This function cleans df using the following steps :
-    1. Inner join the messages_df and categories_df on the id
-    2. Split all the categories into separate category columns
-    3. Rename all the categories columns
-    4. One hot encoding the categories
-    5. From df ,drop the origianl categories column 
-    6. Concatenate categories_df and messages_df 
-    7. Remove all the duplicates
+def load_data(messages_filepath, categories_filepath):
+    ''' This is load data function which is used to load the data from files .
+  
     '''
-    
-    df = messages_df.merge(categories_df, how='inner', on=['id'])
-
-    # create the dataframe
-    categories_df = df.categories.str.split(pat=';', expand=True)
-
-    category_colnames = categories_df.iloc[0].apply(
-        func=lambda x: x[:-2]
-    ).to_list()
-    categories_df.columns = category_colnames
-
-    for column in categories_df:        
-        categories_df[column] = categories_df[column].apply(lambda x: x[-1])        
-        categories_df[column] = categories_df[column].astype(int)
-
-    # from `df` drop original categories column 
-    df.drop(columns=['categories'], inplace=True)    
-    df = pd.concat([df, categories_df], axis=1)
-
-    # Here drop all the duplicates
-    df.drop_duplicates(inplace=True)
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = pd.merge(messages, categories, left_on='id', right_on='id', how='outer')
     return df
 
+def clean_data(df):
+    ''' This is clean data function which helps in cleaning the data.'''
+    categories = df.categories.str.split(';', expand = True)
+    row = categories.loc[0]
+    category_colnames = row.apply(lambda x: x[:-2]).values.tolist()
+    categories.columns = category_colnames
+    categories.related.loc[categories.related == 'related-2'] = 'related-1'
+    for column in categories:
+        categories[column] = categories[column].astype(str).str[-1]
+        categories[column] = pd.to_numeric(categories[column])
+    df.drop('categories', axis = 1, inplace = True)
+    df = pd.concat([df, categories], axis = 1)
+    df.drop_duplicates(subset = 'id', inplace = True)
+    return df
 
 def save_data(df, database_filepath):
-    '''Save the data into database'''
-    engine = create_engine('sqlite:///'+database_filepath)
-    filename = database_filepath.split("/")
-    df.to_sql(filename[1][:-3], engine, index=False)
-
+    ''' This is save data function where we link the datafram to  the sqlite and save it in the database.'''
+    engine = create_engine('sqlite:///' + database_filepath)
+    df.to_sql('FigureEight', engine, index=False)
 
 def main():
-    '''This main function performs the following steps :
-    1. Load in data from the .csv files
-    2. Clean the data
-    3. Save these cleaned data into database
+    ''' This is main data processing function which grab the paths of files .
     '''
     if len(sys.argv) == 4:
 
         messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
         print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
               .format(messages_filepath, categories_filepath))
-        messages_df, categories_df = load_data(messages_filepath, categories_filepath)
-        print('Cleaning data...')
-        df = clean_data(messages_df, categories_df)
-        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        df = load_data(messages_filepath, categories_filepath)
+
+
+        df = clean_data(df)
+
+
         save_data(df, database_filepath)
-        print('Cleaned data saved to database!')
-        
+
+        print('database saved!')
+
     else:
-        print('Provide filepaths of the categories and messages '\
-              'datasets as first and second argument respectively, as '\
-              'well as the filepath of the database to save the cleaned data '\
-              'to as third argument. \n\nExample: python process_data.py '\
-              'disaster_messages.csv disaster_categories.csv '\
-              'DisasterResponse.db')
+        print('Pmissing paths!')
 
 
 if __name__ == '__main__':
