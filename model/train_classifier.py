79eceb5e1b5f73356eb9ea4bb4dda18b1e457b92
Sidharth Kumar Mohanty <65949218+sidharth178@users.noreply.github.com>
2021-09-07 06:02:40 +0530

    Add files via upload

--- model/train_classifier.py
+++ model/train_classifier.py
@@ -1,9 +1,13 @@
-#import all libraries here
+# Disaster Response Pipeline
+# Author: Sidharth Kumar Mohanty
+
+#import necessary libraries
 import sys
 import nltk
 import re
 nltk.download(['punkt', 'wordnet'])
 import warnings
+import pickle
 import pandas as pd
 from sqlalchemy import create_engine
 from nltk.tokenize import word_tokenize
@@ -12,23 +16,29 @@ from sklearn.multioutput import MultiOutputClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
-from sklearn.externals import joblib
-from sklearn.metrics import classification_report, accuracy_score
-from sklearn.multiclass import OneVsRestClassifier
-from sklearn.svm import LinearSVC
+from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import RandomForestClassifier
+
 
 def load_data(database_filepath):
-    '''Here we loaded the data in this function from the database'''
-    engine = create_engine('sqlite:///'+ database_filepath)
-    df = pd.read_sql_table('FigureEight', engine)
-    X = df.message.values
-    Y = df[df.columns[4:]].values
-    category_names = list(df.columns[4:])
-    return X, Y, category_names
+    '''Load the created database'''
+    engine = create_engine('sqlite:///'+database_filepath)
+    df = pd.read_sql_table('DisasterResponse', engine)
+    X = df.message
+    y = df[df.columns[4:]]
+    category_names = y.columns
+    return X, y, category_names 
+
 
 def tokenize(text):
-    ''' This is tokenizing function'''
-    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
+    # remove URL present in the messages
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    detected_urls = re.findall(url_regex, text)
+    for url in detected_urls:
+        text = text.replace(url, "urlplaceholder")
+
+    # convert text messages into tokens
     tokens = word_tokenize(text)
     lemmatizer = WordNetLemmatizer()
 
@@ -36,57 +46,67 @@ def tokenize(text):
     for tok in tokens:
         clean_tok = lemmatizer.lemmatize(tok).lower().strip()
         clean_tokens.append(clean_tok)
-    return clean_tokens
+
+    return clean_tokens    
 
 
 def build_model():
-    '''This is build model function.The output of this function 
-    is a Scikit ML Pipeline which process text message and it
-    also apply a classifier'''
+    ''' Create a pipeline for this model '''
     pipeline = Pipeline([
         ('vect', CountVectorizer(tokenizer=tokenize)),
         ('tfidf', TfidfTransformer()),
-        ('clf', MultiOutputClassifier(OneVsRestClassifier(LinearSVC(random_state = 0))))
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))
     ])
+    
     parameters = {
-                'tfidf__smooth_idf':[True, False],
-                'clf__estimator__estimator__C': [1, 2, 5]
-             }
-    cv = GridSearchCV(pipeline, param_grid=parameters, scoring='precision_samples', cv = 5)
-    return cv
+        'clf__estimator__n_estimators': [8, 15],
+        'clf__estimator__min_samples_split': [2],
+    
+    }
+    model = GridSearchCV(pipeline, param_grid=parameters, n_jobs=4, verbose=2, cv=3)
+    return model
 
-def evaluate_model(model, X_test, Y_test, category_names):
-    '''This is model Evaluation function which print out model performance'''
-    Y_pred = model.predict(X_test)
-    print(classification_report(Y_test, Y_pred, target_names = category_names))
+
+def evaluate_model(model, X_test, y_test, category_names):
+    '''Evaluate the model performance'''
+    y_pred = model.predict(X_test)
+    print(classification_report(y_test, y_pred, target_names=category_names))
+    
     for i in range(Y_test.shape[1]):
-        print('%25s accuracy : %.2f' %(category_names[i], accuracy_score(Y_test[:,i], Y_pred[:,i])))
+        print('%25s accuracy : %.2f' %(category_names[i], accuracy_score(Y_test[:,i], y_pred[:,i])))
 
 def save_model(model, model_filepath):
-    '''This function save the model as a pickle file.'''
-    joblib.dump(model, model_filepath)
+    '''save the model.'''
+    with open(model_filepath, 'wb') as file:
+        pickle.dump(model, file)
 
 
 def main():
-    '''This function pass  Path to the Database and Model. '''
     if len(sys.argv) == 3:
         database_filepath, model_filepath = sys.argv[1:]
-        print('Loading the data...\n    DATABASE: {}'.format(database_filepath))
-        X, Y, category_names = load_data(database_filepath)
-        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, y, category_names = load_data(database_filepath)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+        
+        print('Building model...')
         model = build_model()
-        model.fit(X_train, Y_train)
+        
+        print('Training model...')
+        model.fit(X_train, y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, y_test, category_names)
 
-        print('Evaluating the model...')
-        evaluate_model(model, X_test, Y_test, category_names)
-
-        print('Saving the model...\n    MODEL: {}'.format(model_filepath))
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
         save_model(model, model_filepath)
 
         print('Trained model saved!')
 
     else:
-        print('missing paths!!')
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
 
 
 if __name__ == '__main__':
