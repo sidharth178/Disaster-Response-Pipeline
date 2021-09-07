c2fe96d4b7941e61de10041e86c45d3c03c82d46
Sidharth Kumar Mohanty <65949218+sidharth178@users.noreply.github.com>
2021-09-07 05:53:43 +0530

    Add files via upload

--- app/run.py
+++ app/run.py
@@ -1,40 +1,22 @@
-# import all libraries here
 import json
 import plotly
 import pandas as pd
-import nltk
+
 from nltk.stem import WordNetLemmatizer
-from nltk.tokenize import word_tokenize, sent_tokenize
-from nltk import pos_tag, word_tokenize
-from sklearn.base import BaseEstimator, TransformerMixin
+from nltk.tokenize import word_tokenize
+
 from flask import Flask
 from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
-from sklearn.externals import joblib
+import sklearn.externals
+import joblib
 from sqlalchemy import create_engine
+import warnings
+warnings.filterwarnings("ignore")
 
 app = Flask(__name__)
 
-class StartingVerbExtractor(BaseEstimator, TransformerMixin):
-    def starting_verb(self, text):
-        ''' We will extract verbs from the text after tokenizing them'''
-        li = nltk.sent_tokenize(text)
-        for sentence in li:
-            pos_tags = nltk.pos_tag(tokenize(sentence))
-            first_word, first_tag = pos_tags[0]
-            if first_tag in ['VB', 'VBP'] or first_word == 'RT':
-                return True
-        return False
-
-    def fit(self, X, y=None):
-        return self
-
-    def transform(self, X):
-        X_tagged = pd.Series(X).apply(self.starting_verb)
-        return pd.DataFrame(X_tagged)
-
 def tokenize(text):
-    '''In this function we will tokenize the text'''
     tokens = word_tokenize(text)
     lemmatizer = WordNetLemmatizer()
 
@@ -44,29 +26,38 @@ def tokenize(text):
         clean_tokens.append(clean_tok)
 
     return clean_tokens
-#Here we lode the data
-engine = create_engine('sqlite:///../data/DisasterResponse.db')
-df = pd.read_sql_table('df', engine)
 
-#Here we load the model!
+# load data
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
+df = pd.read_sql_table('DisasterResponse', engine)
+
+# load model
 model = joblib.load("../models/classifier.pkl")
 
 
-# index webpage receives the user input text for the model and displays the cool visuals 
+# index webpage displays cool visuals and receives user input text for model
 @app.route('/')
 @app.route('/index')
 def index():
-    ''' Here we extract data that needed for visuals! '''
+    
+    # extract data needed for visuals
+    # TODO: Below is an example - modify to extract data for your own visuals
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
     
-    category_names = df.iloc[:,4:].columns
-    category_boolean = (df.iloc[:,4:] != 0).sum().values
+    # category data for plotting
+    categories =  df[df.columns[4:]]
+    cate_counts = (categories.mean()*categories.shape[0]).sort_values(ascending=False)
+    cate_names = list(cate_counts.index)
     
-# here create the visuals    
-   
+    # Plotting of Categories Distribution in Direct Genre
+    direct_cate = df[df.genre == 'direct']
+    direct_cate_counts = (direct_cate.mean()*direct_cate.shape[0]).sort_values(ascending=False)
+    direct_cate_names = list(direct_cate_counts.index)
+    
+    # create visuals
+    # TODO: Below is an example - modify to create your own visuals
     graphs = [
-
         {
             'data': [
                 Bar(
@@ -76,7 +67,7 @@ def index():
             ],
 
             'layout': {
-                'title': ' Message Genres',
+                'title': 'Distribution of Message Genres',
                 'yaxis': {
                     'title': "Count"
                 },
@@ -85,43 +76,66 @@ def index():
                 }
             }
         },
-  
+        # category plotting (Visualization#2)
         {
             'data': [
                 Bar(
-                    x=category_names,
-                    y=category_boolean
+                    x=cate_names,
+                    y=cate_counts
                 )
             ],
 
             'layout': {
-                'title': 'Message Categories',
+                'title': 'Distribution of Message Categories',
                 'yaxis': {
                     'title': "Count"
                 },
                 'xaxis': {
-                    'title': "Category",
-                    'tickangle': 35
+                    'title': "Categories"
+                }
+            }
+            
+        },
+        # Categories Distribution in Direct Genre (Visualization#3)
+        {
+            'data': [
+                Bar(
+                    x=direct_cate_names,
+                    y=direct_cate_counts
+                )
+            ],
+
+            'layout': {
+                'title': 'Categories Distribution in Direct Genre',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Categories in Direct Genre"
                 }
             }
         }
     ]
     
-    # Here we encoded the plotly graphs in JSON
+    # encode plotly graphs in JSON
     ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
     graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
     
+    # render web page with plotly graphs
     return render_template('master.html', ids=ids, graphJSON=graphJSON)
 
+
+# web page that handles user query and displays model results
 @app.route('/go')
 def go():
-    '''Will push the Query through this function '''
+    # save user input in query
     query = request.args.get('query', '') 
 
+    # use model to predict classification for query
     classification_labels = model.predict([query])[0]
     classification_results = dict(zip(df.columns[4:], classification_labels))
-    
-   
+
+    # This will render the go.html Please see that file. 
     return render_template(
         'go.html',
         query=query,
@@ -130,7 +144,8 @@ def go():
 
 
 def main():
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    
+    app.run(debug=False)
 
 
 if __name__ == '__main__':
