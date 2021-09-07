2ceffed6de6de91edd6a8f949baa3a0400039135
Sidharth Kumar Mohanty <65949218+sidharth178@users.noreply.github.com>
2021-09-08 03:24:25 +0530

    Add files via upload

--- app/run.py
+++ app/run.py
@@ -32,7 +32,7 @@ engine = create_engine('sqlite:///../data/DisasterResponse.db')
 df = pd.read_sql_table('DisasterResponse', engine)
 
 # load model
-model = joblib.load("../models/classifier.pkl")
+model = joblib.load("../model/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
